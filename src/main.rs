0b0cf3ea3f7bd3f8592c04e7cfc39ebeeef366ba
ravi maithrey <maitrey.ind@gmail.com>
2021-08-13 16:28:51 +0530

    basic example ran

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use rust_bert::pipelines::question_answering::{QuestionAnsweringModel, QaInput};
+use anyhow;
+
+fn main() -> anyhow::Result<()> {
+    let qa_model = QuestionAnsweringModel::new(Default::default())?;
+    let question = String::from("Where does Amy live ?");
+    let context = String::from("Amy lives in Amsterdam");
+
+    let answers = qa_model.predict(&[QaInput { question, context }], 1, 32);
+    println!("{:?}", answers);
+    Ok(())
 }
